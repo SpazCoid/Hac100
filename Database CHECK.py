aa19196d1308e4a61419de2db54be34a32f8932e
SpazCoidN <47160653+NathanAinsley@users.noreply.github.com>
2019-10-27 11:49:06 +0000

    Merge

--- Database CHECK.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('UserDB.db')
-cursor = conn.execute("SELECT * from Users")
-
-records=cursor.fetchall()
-
-for record in records:
-   print("UserID = ", record[0], "Username = ", record[1] , "Pass = ", record[2], "ADD = ", record[3],"POSTC = ", record[4], "PHONUM = ", record[5], "Notif Med = ", record[6], "Notif News = ", record[7])
--- GUI.py
+++ GUI.py
@@ -121,6 +121,8 @@ def AddNewAcc():
     user_hash = hashlib.sha256(str(user).encode()).hexdigest()
     user_id = user_hash[:8]
     user_pwd = hashlib.sha256(str(pwd).encode()).hexdigest()
+    
+#---CREATING ACCOUNT ACCESS FRAME
 
     conn = sqlite3.connect("UserDB.db")
     cursor = conn.cursor()
@@ -130,8 +132,8 @@ def AddNewAcc():
     Login()
 
 def AccountAccessMain():
-    
-    LoggedIn = user_LoggedInvar
+    user = userID
+    LoggedIn = user_LoggedINvar
     LogVar = ("Currently Logged in as:", LoggedIn)
 #---CREATING ACCOUNT ACCESS FRAME
     frameAccMenu = Frame(root)
@@ -149,7 +151,7 @@ def AccountAccessMain():
     frameButtonAS.configure(background="#cdfbff")
 
     #---BUTTON TO REDIRECT USER TO MEDICATION ENTRY SCREEN
-    buttn1 = Button(frameButtonAS, height=5, width=16,background="#7f82ff" ,text = "Medication Menu", command=lambda: [MedMenu(), frameAccMenu.destroy(), frameButtonAS.destroy()]).grid(row=1,column=0)
+    buttn1 = Button(frameButtonAS, height=5, width=16,background="#7f82ff" ,text = "Medication Menu", command=lambda: [frameAccMenu.destroy(), frameButtonAS.destroy(), MedMenu()]).grid(row=1,column=0)
  
     #---SPACE
     Label(frameButtonAS, text="\t\t", background="#cdfbff").grid(row=2,column=0,sticky=NSEW)   
@@ -163,8 +165,61 @@ def AccountAccessMain():
     #---BUTTON TO REDIRECT USER TO ACCOUNT CREATION
     buttn3 = Button(frameButtonAS,height=5, width=16 ,background="#7f82ff" ,text = "Log Out", command=lambda: [StartUp(), frameAccMenu.destroy(), frameButtonAS.destroy()]).grid(row=5,column=0)
 
+def MedMenu():
+    user = userID
+    frameMedMenu = Frame(root)
+    frameMedMenu.pack()
+    frameMedMenu.configure(background="#cdfbff")
+    Label(frameMedMenu, text="User Medication Menu",font=("Helvetica 35 bold") , background = "#cdfbff").grid(row=1,column=0, sticky=NSEW)
+
+#---SPACE
+    Label(frameMedMenu, text="\t\t\t\t", background="#cdfbff").grid(row=2,column=0)
+
+#---DATA ENTRY
+    #---MEDICATION ID ENTRY
+    Label(frameMedMenu, text = "Medication ID", font=("Helvetica 15 bold"), background="#cdfbff").grid(row=3, column=0)
+    MedIDvar = StringVar()
+    MedID = Entry(frameMedMenu, textvariable=MedIDvar).grid(row=3,column=1)
+
+    #---MEDICATION NAME ENTRY
+    Label(frameMedMenu, text = "Medication Name", font=("Helvetica 15 bold"), background="#cdfbff").grid(row=4, column=0)
+    MedNamevar = StringVar()
+    MedName = Entry(frameMedMenu, textvariable=MedNamevar).grid(row=4,column=1)
+
+    #---MEDICATION DESCRIPTION ENTRY
+    Label(frameMedMenu, text = "Medication Description", font=("Helvetica 15 bold"), background="#cdfbff").grid(row=5, column=0)
+    MedDescvar = StringVar()
+    MedDesc = Entry(frameMedMenu, textvariable=MedDescvar).grid(row=5,column=1)
+
+    #---MEDICATION DOSE ENTRY
+    Label(frameMedMenu, text = "Medication Dose", font=("Helvetica 15 bold"), background="#cdfbff").grid(row=6, column=0)
+    MedDosevar = StringVar()
+    MedDose = Entry(frameMedMenu, textvariable=MedDosevar).grid(row=6,column=1)
+
+    frameButtonMA = Frame(root)
+    frameButtonMA.pack()
+    frameButtonMA.configure(background="#cdfbff")
+
+#---BUTTON TO REDIRECT USER TO ACCOUNT CREATION
+    buttn3 = Button(frameButtonMA,height=5, width=16 ,background="#7f82ff" ,text = "Log Out", command=lambda: [StartUp(), frameButtonMA.destroy(), frameMedMenu.destroy()]).grid(row=5,column=0)
+
+    i = 1
+    conn=sqlite3.connect("UserDB.db")
+    cur = conn.cursor()
+    cur.execute("SELECT Medication.MedID, Medication.MedicationName, Medication.MedicationDesc , Medication.MedicationDose FROM Link, Medication WHERE Link.UserID = (?) AND Medication.MedID = Link.MedID", (user,))
+    records=cur.fetchall()
+    for record in records:
+        print("Medication ID = ", record[0] , "Medication Name = ", record[1] , "Medication Description = ", record[2] , "Medication Dose = ", record[3])
+        listbox.insert(i,"Medication ID = ", record[0] , "Medication Name = ", record[1] , "Medication Description = ", record[2] , "Medication Dose = ", record[3])
+        listbox.insert(i,"----------------------------------------------------------", "\n\n")
+ 
+    listbox = Listbox(frameMedMenu, height=8, width=80).grid(row=7,column=0)
+
+    i += 1
+
+
 def Login():
-    global User_NAMEvar, User_PASSvar, frameLogin, frameButtonL
+    global User_NAMEvar, User_PASSvar, frameLogin, frameButtonL, userID
 
 #---MAIN LOGIN FRAME
     frameLogin = Frame(root)
@@ -199,7 +254,7 @@ def Login():
     buttn2 = Button(frameButtonL,text="Cancel", background="#7f82ff", command=lambda: [StartUp(), frameLogin.destroy(), frameButtonL.destroy()]).grid(row=1,column=2)
     
 def LoginCheck():
-    global userID, user_LoggedInvar, frameLogin, frameButtonL
+    global userID, user_LoggedINvar, frameLogin, frameButtonL
 #---GRABS USER AND PASSWORD VARIABLES FROM INPUT BOX
     user = User_NAMEvar.get()
     pwd = User_PASSvar.get()
@@ -213,29 +268,18 @@ def LoginCheck():
 #---GRAB DETAILS FROM THE DATABASE
     conn = sqlite3.connect("UserDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT count(*) FROM Users where Username=(?)", (user,))
-    records=cursor.fetchone()
+    cursor.execute("SELECT UserID, Username, UserPass FROM Users where Username=(?)", (user,))
+    records = cursor.fetchall()
 
-    if records == 0:
-        messagebox.showerror("Error", "Username / Password is incorrect")
-    if records == 1:
-        for record in records:
-            if user == record[0]:
-                conn = sqlite3.connect("UserDB.db")
-                cursor = conn.cursor()
-                cursor.execute("SELECT UserID, Username, UserPass FROM Users where Username=(?)", (user,))
-                records.fetchall()
-
-                for record in records:
-                    if key == record[2]:
-                        userID = record[0]
-                        user_LoggedInvar = user
-                        print(userID)
-                        print(user_LoggedInvar)
-                        frameLogin.destroy(), frameButtonL.destroy(); AccountAccessMain()
-                    if key != record[2]:
-                        messagebox.showerror("Error", "Username / Password is incorrect")
-                    else:
-                        messagebox.showerror("Error", "Username / Password is incorrect")
+    for record in records:
+        if key == record[2]:
+            userID = record[0]
+            user_LoggedINvar = user
+            var = user_LoggedINvar
+            print(userID)
+            print(user_LoggedINvar)
+            frameLogin.destroy(), frameButtonL.destroy(); AccountAccessMain()
+        else:
+            messagebox.showerror("Error", "Username / Password is incorrect")
 
 Root()
