aec7a01f2482b77320a2ebd04653e2485a7ce8f5
SpazCoidN <47160653+NathanAinsley@users.noreply.github.com>
2019-10-27 03:39:25 +0000

    Creates News.db

---
+++ NewsdbMaker.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn=sqlite3.connect("News.db")
+
+conn.execute('''Create Table IF NOT EXISTS News
+             (Title                     TEXT  (50) NOT NULL,
+              Description               TEXT (50)            NOT NULL,
+              URL                       TEXT (50)           NOT NULL,
+              publishedAt               TEXT (15)           NOT NULL,
+              Content                   TEXT (500)           NOT NULL,
+              PRIMARY KEY (Title)
+              UNIQUE(Title,URL)
+              );''')
