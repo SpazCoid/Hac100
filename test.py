07400b53902bc10a0f224e3968654ae310581ae4
SpazCoidJ <50532438+soupfella@users.noreply.github.com>
2019-10-27 05:01:08 +0000

    added notification checkboxes to account creation screen

--- test.py
+++ test.py
@@ -36,22 +36,22 @@ def StartUp():
     frameButtonS.configure(background="#cdfbff")
 
     #---BUTTON TO REDIRECT USER TO LOGIN SCREEN
-    b1 = Button(frameButtonS, height=5, width=16,background="#7f82ff" ,text = "Login", command=lambda: [Login(), frameStart.destroy(), frameButtonS.destroy()]).grid(row=1,column=0)
+    buttn1 = Button(frameButtonS, height=5, width=16,background="#7f82ff" ,text = "Login", command=lambda: [Login(), frameStart.destroy(), frameButtonS.destroy()]).grid(row=1,column=0)
  
     #---SPACE
     Label(frameButtonS, text="\t\t", background="#cdfbff").grid(row=2,column=0,sticky=NSEW)   
 
     #---BUTTON TO REDIRECT USER TO ACCOUNT CREATION
-    b2 = Button(frameButtonS,height=5, width=16 ,background="#7f82ff" ,text = "Create New Account", command=lambda: [AccCreation(), frameStart.destroy(), frameButtonS.destroy()]).grid(row=3,column=0)
+    buttn2 = Button(frameButtonS,height=5, width=16 ,background="#7f82ff" ,text = "Create New Account", command=lambda: [AccCreation(), frameStart.destroy(), frameButtonS.destroy()]).grid(row=3,column=0)
 
     #---SPACE
     Label(frameButtonS, text="\t\t", background="#cdfbff").grid(row=4,column=0,sticky=NSEW)
 
     #---BUTTON TO REDIRECT USER TO ACCOUNT CREATION
-    b3 = Button(frameButtonS,height=5, width=16 ,background="#7f82ff" ,text = "Quit", command=lambda: [quit()]).grid(row=5,column=0)
+    buttn3 = Button(frameButtonS,height=5, width=16 ,background="#7f82ff" ,text = "Quit", command=lambda: [quit()]).grid(row=5,column=0)
 
 def AccCreation():
-    global User_NAMEvar, User_PASSvar, User_ADDRvar, User_POSTCvar, User_PHONUMvar
+    global User_NAMEvar, User_PASSvar, User_ADDRvar, User_POSTCvar, User_PHONUMvar, User_NOTIFMEDvar, User_NOTIFNEWSvar
 #---Account Creation Frame
     frameAccCreate = Frame(root)
     frameAccCreate.pack()
@@ -91,13 +91,23 @@ def AccCreation():
     User_PHONUMvar = StringVar()
     User_PHONUM = Entry(frameAccDet, textvariable=User_PHONUMvar).grid(row=5,column=1)
 
+#---MEDICATION SMS NOTIFICATION SETTING
+    Label(frameAccDet, text = "Medication SMS Notifications (check to turn on)", font=("Helvetica 15 bold"), background="#cdfbff").grid(row=6, column=0)    
+    User_NOTIFMEDvar = IntVar()
+    User_NOTIFMED = Checkbutton(frameAccDet,onvalue=1, offvalue=0, var=User_NOTIFMEDvar).grid(row=6,column=1)
+
+#---NEWS SMS NOTIFICATION SETTING
+    Label(frameAccDet, text = "News SMS Notifications (check to turn on)", font=("Helvetica 15 bold"), background="#cdfbff").grid(row=7, column=0)    
+    User_NOTIFNEWSvar = IntVar()
+    User_NOTIFNEWS = Checkbutton(frameAccDet,onvalue=1, offvalue=0, var=User_NOTIFNEWSvar).grid(row=7,column=1)
+
 #---CREATING THE BUTTON FRAME FOR THE USER TO USE
     frameButtonAC = Frame(root)
     frameButtonAC.pack()
     frameButtonAC.configure(background="#cdfbff")
 
     #---ADD ACCOUNT TO DATABASE
-    b1 = Button(frameButtonAC, height=5, width=16,background="#7f82ff" ,text = "Create Account", command=lambda: [AddNewAcc(), frameAccDet.destroy(), frameButtonAC.destroy()]).grid(row=1,column=0)
+    buttn1 = Button(frameButtonAC, height=5, width=16,background="#7f82ff" ,text = "Create Account", command=lambda: [AddNewAcc(), frameAccDet.destroy(),frameAccCreate.destroy(), frameButtonAC.destroy()]).grid(row=1,column=0)
  
 def AddNewAcc():
     user = User_NAMEvar.get()
@@ -105,6 +115,8 @@ def AddNewAcc():
     addr = User_ADDRvar.get()
     postc = User_POSTCvar.get()
     phonum = User_PHONUMvar.get()
+    notifm = User_NOTIFMEDvar.get()
+    notifn = User_NOTIFNEWSvar.get()
 
     user_hash = hashlib.sha256(str(user).encode()).hexdigest()
     user_id = user_hash[:8]
@@ -112,14 +124,47 @@ def AddNewAcc():
 
     conn = sqlite3.connect("UserDB.db")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO Users VALUES (?,?,?,?,?,?)", (user_id,user,user_pwd,addr,postc,phonum))
+    cursor.execute("INSERT INTO Users VALUES (?,?,?,?,?,?,?,?)", (user_id,user,user_pwd,addr,postc,phonum,notifm,notifn))
     conn.commit()
     conn.close()
     Login()
 
+def AccountAccessMain():
+    
+    LoggedIn = user_LoggedInvar
+    LogVar = ("Currently Logged in as:", LoggedIn)
+#---CREATING ACCOUNT ACCESS FRAME
+    frameAccMenu = Frame(root)
+    frameAccMenu.pack()
+    frameAccMenu.configure(background="#cdfbff")
+    Label(frameAccMenu, text=LogVar,font=("Helvetica 35 bold") , background = "#cdfbff").grid(row=1,column=0, sticky=NSEW)
+#---SPACE
+    Label(frameAccMenu, text="\t\t", background="#cdfbff").grid(row=2,column=0,sticky=NSEW )
+    Label(frameAccMenu, text="\t\t", background="#cdfbff").grid(row=3,column=0,sticky=NSEW )
+    Label(frameAccMenu, text="\t\t", background="#cdfbff").grid(row=4,column=0,sticky=NSEW )
+
+#---CREATING THE BUTTON FRAME FOR THE USER TO USE
+    frameButtonAS = Frame(root)
+    frameButtonAS.pack()
+    frameButtonAS.configure(background="#cdfbff")
+
+    #---BUTTON TO REDIRECT USER TO MEDICATION ENTRY SCREEN
+    buttn1 = Button(frameButtonAS, height=5, width=16,background="#7f82ff" ,text = "Medication Menu", command=lambda: [MedMenu(), frameAccMenu.destroy(), frameButtonAS.destroy()]).grid(row=1,column=0)
+ 
+    #---SPACE
+    Label(frameButtonAS, text="\t\t", background="#cdfbff").grid(row=2,column=0,sticky=NSEW)   
+
+    #---BUTTON TO REDIRECT USER TO ACCOUNT CREATION
+    buttn2 = Button(frameButtonAS,height=5, width=16 ,background="#7f82ff" ,text = "Notifications", command=lambda: [NotifToggle(), frameAccMenu.destroy(), frameButtonAS.destroy()]).grid(row=3,column=0)
+
+    #---SPACE
+    Label(frameButtonAS, text="\t\t", background="#cdfbff").grid(row=4,column=0,sticky=NSEW)
+
+    #---BUTTON TO REDIRECT USER TO ACCOUNT CREATION
+    buttn3 = Button(frameButtonAS,height=5, width=16 ,background="#7f82ff" ,text = "Log Out", command=lambda: [StartUp(), frameAccMenu.destroy(), frameButtonAS.destroy()]).grid(row=5,column=0)
 
 def Login():
-    global User_NAMEvar, User_PASSvar, frameLogin, frameLoginButtons
+    global User_NAMEvar, User_PASSvar, frameLogin, frameButtonL
 
 #---MAIN LOGIN FRAME
     frameLogin = Frame(root)
@@ -149,11 +194,12 @@ def Login():
     Label(frameLogin, text="\t\t", background="#cdfbff").grid(row=5,column=0, )
     
     #7f82ff - Colour for buttons
-    b1 = Button(frameButtonL,text="Login",background="#7f82ff", command=lambda: [LoginCheck(), frameLogin.destroy(), frameButtonL.destroy()]).grid(row=1,column=0)
+    buttn1 = Button(frameButtonL,text="Login",background="#7f82ff", command=lambda: [LoginCheck(), frameLogin.destroy(), frameButtonL.destroy()]).grid(row=1,column=0)
     Label(frameButtonL, text="      ", background="#cdfbff").grid(row=1,column=1)
-    b2 = Button(frameButtonL,text="Cancel", background="#7f82ff", command=lambda: [StartUp(), frameLogin.destroy(), frameButtonL.destroy()]).grid(row=1,column=2)
+    buttn2 = Button(frameButtonL,text="Cancel", background="#7f82ff", command=lambda: [StartUp(), frameLogin.destroy(), frameButtonL.destroy()]).grid(row=1,column=2)
     
 def LoginCheck():
+    global userID, user_LoggedInvar, frameLogin, frameButtonL
 #---GRABS USER AND PASSWORD VARIABLES FROM INPUT BOX
     user = User_NAMEvar.get()
     pwd = User_PASSvar.get()
@@ -167,18 +213,29 @@ def LoginCheck():
 #---GRAB DETAILS FROM THE DATABASE
     conn = sqlite3.connect("UserDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT UserID, Username, UserPass FROM Users")
+    cursor.execute("SELECT count(*) FROM Users where Username=(?)", (user,))
+    records=cursor.fetchone()
 
-    records=cursor.fetchall()
+    if records == 0:
+        messagebox.showerror("Error", "Username / Password is incorrect")
+    if records == 1:
+        for record in records:
+            if user == record[0]:
+                conn = sqlite3.connect("UserDB.db")
+                cursor = conn.cursor()
+                cursor.execute("SELECT UserID, Username, UserPass FROM Users where Username=(?)", (user,))
+                records.fetchall()
 
-    for record in records:
-        if user == record[1] and key == record[2]:
-            Root()
-            frameLogin.destroy()
-            frameButtonL.destroy()
-            break
-        else:
-            messagebox.showerror("Error", "Username / Password is incorrect")
-        
+                for record in records:
+                    if key == record[2]:
+                        userID = record[0]
+                        user_LoggedInvar = user
+                        print(userID)
+                        print(user_LoggedInvar)
+                        frameLogin.destroy(), frameButtonL.destroy(); AccountAccessMain()
+                    if key != record[2]:
+                        messagebox.showerror("Error", "Username / Password is incorrect")
+                    else:
+                        messagebox.showerror("Error", "Username / Password is incorrect")
 
 Root()
